8391042af5d56400dcf91a9cdf6a482f2028d7a4
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2023-03-08 13:16:25 -0500

    Omer I added some stuff

---
+++ Parallel/src/zork/commands/Attributes.java
@@ -0,0 +1,10 @@
+package zork.commands;
+
+public class Attributes {
+    int Intellect;
+    int Psyche;
+    int Physique;
+    int Motorics;
+
+    
+}
---
+++ Parallel/src/zork/commands/Drop.java
@@ -0,0 +1,9 @@
+package zork.commands;
+import zork.data.*;
+import zork.*;
+
+public class Drop extends Item {
+    public Drop(int weight, String name, boolean isOpenable) {
+        super(weight, name, isOpenable);
+    }
+}

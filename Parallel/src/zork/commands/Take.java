b87dc17e19aecc989c5b7fe62fb57d924a64db2b
omerhussein9 <ohussein@bayviewglen.ca>
2023-03-08 12:26:14 -0500

    owen use the take.java file for this

---
+++ Parallel/src/zork/commands/Take.java
@@ -0,0 +1,10 @@
+package zork.commands;
+
+import zork.data.*;
+import zork.*;
+
+public class Take extends Item {
+    public Take(int weight, String name, boolean isOpenable) {
+        super(weight, name, isOpenable);
+    }
+}

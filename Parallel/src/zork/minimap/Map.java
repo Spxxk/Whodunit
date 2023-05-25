b8aff27fe86681bdab5f237fb2d9b55bd0e54df0
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2023-05-25 12:46:23 -0400

    we did it we did it we did it horray vamasimos

--- Parallel/src/zork/minimap/Map.java
+++ Parallel/src/zork/minimap/Map.java
@@ -2,31 +2,41 @@ package zork.minimap;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 
 public class Map extends JFrame {
-    
+
     private static final int MAP_WIDTH = 2000;
     private static final int MAP_HEIGHT = 2000;
     private static final double MINIMAP_SCALE = 0.1;
+    private static final int PLAYER_SIZE = 10;
+
+    private Player player;
+    private double mouseX;
+    private double mouseY;
+    private BufferedImage map;
+    private BufferedImage miniMap;
 
     public Map() {
-        BufferedImage map = new BufferedImage(MAP_WIDTH, MAP_HEIGHT, BufferedImage.TYPE_INT_ARGB);
-        BufferedImage miniMap = new BufferedImage((int) (MAP_WIDTH * MINIMAP_SCALE), (int) (MAP_HEIGHT * MINIMAP_SCALE), BufferedImage.TYPE_INT_ARGB);
+        player = new Player(0, 0); // player starts at the top left of the map
 
-        drawMap(map);
+        map = new BufferedImage(MAP_WIDTH, MAP_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        drawMap(map.createGraphics());
 
-        Graphics g = miniMap.getGraphics();
-        g.drawImage(map, 0, 0, miniMap.getWidth(), miniMap.getHeight(), null);
-
+        miniMap = new BufferedImage((int) (MAP_WIDTH * MINIMAP_SCALE), (int) (MAP_HEIGHT * MINIMAP_SCALE), BufferedImage.TYPE_INT_RGB);
+        miniMap.createGraphics().drawImage(map, 0, 0, miniMap.getWidth(), miniMap.getHeight(), this);
+        
         JPanel panel = new JPanel() {
             @Override
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(map, (int) (-mouseX / MINIMAP_SCALE + getWidth() / 2), (int) (-mouseY / MINIMAP_SCALE + getHeight() / 2), null);
-                g.drawImage(miniMap, 700, 10, null);
+                g.drawImage(map, (int) (-mouseX / MINIMAP_SCALE + getWidth() / 2), (int) (-mouseY / MINIMAP_SCALE + getHeight() / 2), this);
+                g.drawImage(miniMap, 700, 10, this);
+                g.setColor(Color.RED);
+                g.drawString("Currently here", 700 + (int) (player.getX() * MINIMAP_SCALE), 10 + (int) (player.getY() * MINIMAP_SCALE));
+                g.drawRect(700 + (int) (player.getX() * MINIMAP_SCALE), 10 + (int) (player.getY() * MINIMAP_SCALE), PLAYER_SIZE, PLAYER_SIZE);
             }
         };
 
@@ -34,9 +44,15 @@ public class Map extends JFrame {
         panel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                mouseX = e.getX();
-                mouseY = e.getY();
-                panel.repaint();
+                if (e.getX() >= 700 && e.getX() <= 700 + MAP_WIDTH * MINIMAP_SCALE && e.getY() >= 10 && e.getY() <= 10 + MAP_HEIGHT * MINIMAP_SCALE) {
+                    player.setX((int) ((e.getX() - 700) / MINIMAP_SCALE));
+                    player.setY((int) ((e.getY() - 10) / MINIMAP_SCALE));
+                    map = new BufferedImage(MAP_WIDTH, MAP_HEIGHT, BufferedImage.TYPE_INT_RGB);
+                    drawMap(map.createGraphics());
+                    miniMap = new BufferedImage((int) (MAP_WIDTH * MINIMAP_SCALE), (int) (MAP_HEIGHT * MINIMAP_SCALE), BufferedImage.TYPE_INT_RGB);
+                    miniMap.createGraphics().drawImage(map, 0, 0, miniMap.getWidth(), miniMap.getHeight(), panel);
+                    panel.repaint();
+                }
             }
         });
 
@@ -57,17 +73,15 @@ public class Map extends JFrame {
         setVisible(true);
     }
 
-    private double mouseX;
-    private double mouseY;
-
-    private void drawMap(BufferedImage image) {
-        Graphics2D g = image.createGraphics();
+    private void drawMap(Graphics2D g) {
         g.setColor(Color.GREEN);
         for (int i = 0; i < MAP_WIDTH; i += 100) {
             for (int j = 0; j < MAP_HEIGHT; j += 100) {
                 g.fillRect(i, j, 50, 50);
             }
         }
+        g.setColor(Color.RED);
+        g.fillRect(player.getX(), player.getY(), PLAYER_SIZE, PLAYER_SIZE);
         g.dispose();
     }
 
---
+++ Parallel/src/zork/minimap/Player.java
@@ -0,0 +1,28 @@
+package zork.minimap;
+
+public class Player {
+
+    private int x;
+    private int y;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}

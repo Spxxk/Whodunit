2935320719ccbbbf33c34c6ae8c315a1717920ae
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2023-05-27 22:09:32 -0400

    a

--- Parallel/src/zork/commands/Use.java
+++ Parallel/src/zork/commands/Use.java
@@ -1,5 +1,6 @@
 package zork.commands;
 
+import java.util.Scanner;
 import zork.Constants.ArgumentCount;
 import zork.proto.Command;
 import zork.proto.Item;
@@ -12,12 +13,34 @@ public class Use extends Command {
     public void runCommand(String... args) {
         // TODO Auto-generated method stub
         String argsString = Item.arrayToString(args);
+
         if(argsString.equalsIgnoreCase("gun")){
-            System.out.println("Command gun executed successfully!");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What do you want to do with the gun? Type 'reload', 'shoot', or 'throw'.");
+            String gunCommand = scanner.nextLine();
+
+            if(gunCommand.equalsIgnoreCase("reload")){
+                System.out.println("You reload the gun.");
+                //Bullets++; there is no counter
+            }
+            else if(gunCommand.equalsIgnoreCase("shoot")){
+                System.out.println("You shoot the gun.");
+                //Bullet--; there is no counter
+            }
+            else if(gunCommand.equalsIgnoreCase("throw")){
+                System.out.println("You throw the gun.");
+                //Gun--; there is no gun yet 
+            }
+            else {
+                System.out.println("Invalid command for gun.");
+            }
         }
-        else
-        {
-        throw new UnsupportedOperationException("Unimplemented method 'runCommand'");
+        
+        else if(argsString.equalsIgnoreCase("key")){
+            System.out.println("Command key executed successfully!");
+        }
+        else {
+            throw new UnsupportedOperationException("Unimplemented method 'runCommand'");
         }
     }
 }

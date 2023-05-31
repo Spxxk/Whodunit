6d91675c16178cc7e7dfb41d36082c61f96699bd
omerhussein9 <ohussein@bayviewglen.ca>
2023-05-31 14:55:22 -0400

    FINISHED THE 3RD MINIGAME (caps)

--- Parallel/src/zork/minigames/MemoryNumbers.java
+++ Parallel/src/zork/minigames/MemoryNumbers.java
@@ -1,16 +1,63 @@
 package zork.minigames;
 
 import java.util.Scanner;
-import zork.utils.TimeController;
 
 public class MemoryNumbers {
 
+    public static Scanner in;
+    public static String nums;
+    public static int score;
+    public static boolean finished;
+
+    public static final String RED = "\u001B[31m", WHITE = "\u001B[0m";
+
     public static void play() {
-        printLines();
+        in = new Scanner(System.in);
+
+        System.out.println("Welcome to Memory Numbers! Get a higher score than MR MINATO to win.");
+        System.out.printf("To play, write the number in %sRED%2s once the console tells you to.%n", RED, WHITE);
+        System.out.printf("Type start once you have understood the rules: ");
+
+        nums = "";
+        finished = false;
+        score = 1;
+
+        if(in.nextLine().equalsIgnoreCase("start")) {
+            System.out.println("\nRound 1");
+            while(!finished) { nextRound(); }
+        }
+    }
+
+    private static void nextRound() {
+        try {
+            nums += (int) (Math.random() * 10);
+
+            System.out.println(RED + nums + WHITE);
+            Thread.sleep(1000);
+            printLines();
+            
+            System.out.print("Repeat number here: ");
+
+            String input;
+            if(in.hasNextLine()) { 
+                input = in.nextLine();
+
+                if(!input.equals(nums)) {
+                    System.out.printf("Correct number was [%s], you recieved a score of %s.%n", RED + nums + WHITE, score);
+                    finished = true;
+                    return;
+                }
+
+                score++;
+                System.out.printf("Round %s%n", score);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void printLines() {
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
     
 }

1ce68985e3111db387fa160d261f90880bbd539f
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2023-05-31 19:14:08 -0400

    FIXING WEICHENGS MISTAKE

--- Parallel/src/zork/utils/MinigameLoader.java
+++ Parallel/src/zork/utils/MinigameLoader.java
@@ -1,5 +1,6 @@
 package zork.utils;
 
+import java.lang.reflect.Constructor;
 import java.util.*;
 
 import zork.exceptions.*;
@@ -13,7 +14,9 @@ public class MinigameLoader {
             Class<?>[] classes = Loader.getClasses("zork.minigames");
 
             for (Class<?> c : classes) {
-                Minigame game = (Minigame) c.getConstructors()[0].newInstance();
+                Constructor ctor = c.getConstructors()[0];
+                ctor.trySetAccessible();
+                Minigame game = (Minigame) ctor.newInstance();
                 minigames.put(game.getName().toLowerCase(), game);
             }
         } catch (Exception e) {

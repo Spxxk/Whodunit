837b9d1730243f08123ded6fcbb24eb7aee6d326
Max <57689155+dizmac@users.noreply.github.com>
2023-06-01 11:24:23 -0400

    Owen please pull the code before commiting

--- Parallel/src/zork/minigames/Poker.java
+++ Parallel/src/zork/minigames/Poker.java
@@ -12,6 +12,8 @@ public class Poker extends Minigame {
     private List<Player> npcs;
     private Deck deck;
     private int pot;
+    private int currentBet;
+    private int roundCount;
     private Scanner scanner = new Scanner(System.in);
 
     public Poker() {
@@ -24,6 +26,8 @@ public class Poker extends Minigame {
             this.npcs.add(new Player("NPC" + i, 250));
         }
         this.pot = 0;
+        this.currentBet = 0;
+        this.roundCount = 0;
     }
 
     public void startGame(String... args) {
@@ -49,67 +53,175 @@ public class Poker extends Minigame {
         }
 
         System.out.println("Your hand: " + this.user.getHand());
-        System.out.println("Your bank: " + this.user.getBank());
+        System.out.println("Your bank: $" + this.user.getBank());
 
         for (Player npc : npcs) {
             System.out.println(npc.getName() + "'s hand: " + String.join(", ", Collections.nCopies(2, "-----")));
-            System.out.println(npc.getName() + "'s bank: " + npc.getBank());
+            System.out.println(npc.getName() + "'s bank: $" + npc.getBank());
         }
 
         npcs.get(0).bet(5);
         npcs.get(1).bet(10);
-        pot += 15;
+        this.pot += 15;
+        this.currentBet = 10;
 
         System.out.println(npcs.get(0).getName() + " posted the small blind of $5. Now has $" + npcs.get(0).getBank());
         System.out.println(npcs.get(1).getName() + " posted the big blind of $10. Now has $" + npcs.get(1).getBank());
 
-        System.out.println("Would you like to 'bet', 'call', or 'raise'? ");
-        String decision = scanner.nextLine().toLowerCase();
+        roundCount++;
 
-        int betAmount;
-        switch(decision) {
-            case "bet":
-                betAmount = getPlayerBet();
-                this.user.bet(betAmount);
-                this.pot += betAmount;
-                System.out.println("You bet $" + betAmount + ". Now you have $" + this.user.getBank());
-                break;
-            case "raise":
-                betAmount = getPlayerBet();
-                this.user.bet(this.pot + betAmount);
-                this.pot += this.pot + betAmount;
-                System.out.println("You raised $" + betAmount + ". Now you have $" + this.user.getBank());
-                break;
-            case "call":
-                this.user.bet(this.pot);
-                System.out.println("You called. Now you have $" + this.user.getBank());
-                break;
-            default:
-                System.out.println("Invalid choice.");
-                return;
+        if (roundCount == 1) {
+            dealFlop();
+        } else {
+            dealAdditionalCards();
         }
 
-        Iterator<Player> it = npcs.iterator();
-        while (it.hasNext()) {
-            Player npc = it.next();
-            if (npc.shouldBet()) {
-                npc.bet(this.pot);
-                System.out.println(npc.getName() + " bet $" + this.pot + ". Now has $" + npc.getBank());
+        while (true) {
+            System.out.println("Would you like to 'bet', 'call', or 'raise'? ");
+            String decision = scanner.nextLine().toLowerCase();
+
+            int betAmount;
+            switch (decision) {
+                case "bet":
+                    betAmount = getPlayerBet();
+                    if (betAmount > this.user.getBank()) {
+                        System.out.println("You don't have enough money to bet $" + betAmount + ".");
+                        continue;
+                    }
+                    this.user.bet(betAmount);
+                    this.pot += betAmount;
+                    this.currentBet = betAmount;
+                    System.out.println("You bet $" + betAmount + ". Now you have $" + this.user.getBank());
+                    break;
+                case "raise":
+                    betAmount = getPlayerBet();
+                    int totalAmount = this.currentBet + betAmount;
+                    if (totalAmount > this.user.getBank()) {
+                        System.out.println("You don't have enough money to raise to $" + totalAmount + ".");
+                        continue;
+                    }
+                    this.user.bet(totalAmount);
+                    this.pot += totalAmount;
+                    this.currentBet = totalAmount;
+                    System.out.println("You raised to $" + totalAmount + ". Now you have $" + this.user.getBank());
+                    break;
+                case "call":
+                    int callAmount = this.currentBet;
+                    if (callAmount > this.user.getBank()) {
+                        System.out.println("You don't have enough money to call $" + callAmount + ".");
+                        continue;
+                    }
+                    this.user.bet(callAmount);
+                    this.pot += callAmount;
+                    System.out.println("You called. Now you have $" + this.user.getBank());
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    continue;
+            }
+
+            for (Player npc : npcs) {
+                try {
+                    Thread.sleep(2000); // Delay between NPC moves
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                int npcBetAmount = getRandomBetAmount(npc);
+                if (npcBetAmount > npc.getBank()) {
+                    npcBetAmount = npc.getBank();
+                }
+                npc.bet(npcBetAmount);
+                this.pot += npcBetAmount;
+                System.out.println(npc.getName() + " bet $" + npcBetAmount + ". Now has $" + npc.getBank());
+            }
+
+            if (roundCount == 1) {
+                dealFlop();
+            } else if (roundCount == 2) {
+                dealTurn();
+            } else if (roundCount == 3) {
+                dealRiver();
             } else {
-                System.out.println(npc.getName() + " folded. Now has $" + npc.getBank());
-                it.remove();
+                break;
             }
         }
 
         Player winner = determineWinner();
 
-        winner.win(this.pot);
-        System.out.println(winner.getName() + " wins the pot of $" + this.pot + ". Now has $" + winner.getBank());
+        System.out.println(winner.getName() + " wins the round!");
+
+        try {
+            Thread.sleep(2000); // Delay before revealing hands
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         for (Player npc : npcs) {
+            try {
+                Thread.sleep(2000); // Delay between revealing each NPC's hand
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(npc.getName() + "'s hand was: " + npc.getHand());
+            System.out.println(npc.getName() + "'s bank: $" + npc.getBank());
         }
-        System.out.println(winner.getName() + " wins the round!");
+
+        try {
+            Thread.sleep(2000); // Delay before displaying user's hand and bank
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Your hand: " + this.user.getHand());
+        System.out.println("Your bank: $" + this.user.getBank());
+
+        winner.win(this.pot);
+        System.out.println(winner.getName() + " wins $" + this.pot + "! Now has $" + winner.getBank());
+
+        this.pot = 0;
+        this.currentBet = 0;
+        this.roundCount = 0;
+    }
+
+    private void dealFlop() {
+        System.out.println("Dealing flop...");
+        for (int i = 0; i < 3; i++) {
+            Card card = this.deck.dealCard();
+            this.user.addCard(card);
+            for (Player npc : npcs) {
+                npc.addCard(card);
+            }
+        }
+        System.out.println("Flop: " + this.user.getHand().subList(2, 5));
+    }
+
+    private void dealAdditionalCards() {
+        Card card = this.deck.dealCard();
+        this.user.addCard(card);
+        for (Player npc : npcs) {
+            npc.addCard(card);
+        }
+        System.out.println("Additional card: " + card);
+    }
+
+    private void dealTurn() {
+        System.out.println("Dealing turn...");
+        Card card = this.deck.dealCard();
+        this.user.addCard(card);
+        for (Player npc : npcs) {
+            npc.addCard(card);
+        }
+        System.out.println("Turn: " + card);
+    }
+
+    private void dealRiver() {
+        System.out.println("Dealing river...");
+        Card card = this.deck.dealCard();
+        this.user.addCard(card);
+        for (Player npc : npcs) {
+            npc.addCard(card);
+        }
+        System.out.println("River: " + card);
     }
 
     private int getPlayerBet() {
@@ -126,6 +238,12 @@ public class Poker extends Minigame {
                 System.out.println("Please enter a valid bet.");
             }
         }
+    }
+
+    private int getRandomBetAmount(Player player) {
+        int maxBet = player.getBank() / 10 * 10;
+        int minBet = 10;
+        return minBet + (int) (Math.random() * ((maxBet - minBet) / 10 + 1)) * 10;
     }
 
     private Player determineWinner() {

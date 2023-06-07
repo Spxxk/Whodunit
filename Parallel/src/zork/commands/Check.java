17f6bb26ae954569181caf4ae92baec74600eeed
omerhussein9 <ohussein@bayviewglen.ca>
2023-06-07 19:00:26 -0400

    added the cellar and some new items

--- Parallel/src/zork/commands/Check.java
+++ Parallel/src/zork/commands/Check.java
@@ -4,6 +4,7 @@ import zork.Game;
 import zork.Constants.ArgumentCount;
 import zork.proto.Command;
 import zork.proto.Item;
+import zork.proto.Exit;
 import zork.utils.CharacterConstants;
 import zork.utils.Give;
 
@@ -21,15 +22,42 @@ public class Check extends Command {
                     Game.print("/bYou found a letter addressed to you from your friend Brent.");
                     if(Give.giveItem(Game.itemList.get("letterAddressedToYou"), null)) 
                         Game.print("/bType /rInventory/b into the console to see your current items.");
+                } else {
+                    Game.print("/b" + itemName + " does not need to be checked.");
                 }
+                break;
             case "Your Bathroom":
                 if(itemName.equalsIgnoreCase("Bathtub")) {
                     if(CharacterConstants.READ_LETTER_FROM_BRENT) {
-                        Game.print("/bYou felt the bathtub looked suspicious, so you checked underneath the bathtub.");
-                        Game.print("/bThere appears to be a trapdoor underneath.");
-                        Game.print("/bMaybe you can get out this way.");
+                        Game.print("/bYou felt the bathtub was the lake from the clue, so you checked underneath it.");
+                        Game.print("/bThere appears to be a trapdoor underneath. Maybe you can get out this way.");
+                        Game.print("/bTo enter the trapdoor, type /rGo Down/b in the console.");
+
+                        Exit e = new Exit("Down", "cellar", false, null);
+                        try {
+                            Game.roomMap.get("userBathroom").addExit(e);
+                        } catch(Exception ex) { Game.print("/rUnknown error has occured, please restart."); }
+                    } else {
+                        Game.print("/bWhat? It's just a normal bathtub.");
                     }
+                } else {
+                    Game.print("/b" + itemName + " does not need to be checked.");
                 }
+                break;
+            case "Cellar":
+                if(itemName.equalsIgnoreCase("Crate")) {
+                    Game.print("/bYou opened the lid of the crate and checked inside.");
+                    Game.print("/bYou found a bullet at the bottom of the crate.");
+                    Give.giveItem(Game.itemList.get("cellarBullet"), null);
+                    Game.print("/bWhy would a bullet be inside this crate? What is this place?");
+                } else {
+                    Game.print("/b" + itemName + " does not need to be checked.");
+                }
+                break;
+
+            default:
+                Game.print("/b" + itemName + " does not need to be checked.");
+                break;
         }
     }
 }

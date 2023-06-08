473b3e50da6aa0a36e97183a5e777391649d375a
Owen Chen <73706094+Spxxk@users.noreply.github.com>
2023-06-08 00:53:03 -0400

    Added dialouge

--- Parallel/src/zork/commands/Go.java
+++ Parallel/src/zork/commands/Go.java
@@ -11,8 +11,10 @@ import zork.proto.Item;
 import zork.utils.CharacterConstants;
 
 public class Go extends Command {
-    
-    public Go() { super("Go", "Go in a certain direction", ArgumentCount.ONE); } // Super constructor (name, arguments)
+
+    public Go() {
+        super("Go", "Go in a certain direction", ArgumentCount.ONE);
+    } // Super constructor (name, arguments)
 
     public void runCommand(String... args) {
         Player player = Game.player; // Player
@@ -21,17 +23,17 @@ public class Go extends Command {
 
         for (int i = 0; i < room.getExits().size(); i++) {
             Exit e = room.getExits().get(i); // Exits of the current room
-            
+
             if (e.getDirection().equalsIgnoreCase(direction)) { // Check if direction is the selected one
-                if(e.isLocked()) { // If it's locked and we have the key, access will be granted, we don't need to "unlock" the room
-                    for(Item item : player.getInventory().getContents()) {
-                        if(item.getId().equalsIgnoreCase(e.getAdjacentRoom()+"Key")) { 
+                if (e.isLocked()) { // If it's locked and we have the key, access will be granted, we don't need to "unlock" the room
+                    for (Item item : player.getInventory().getContents()) {
+                        if (item.getId().equalsIgnoreCase(e.getAdjacentRoom() + "Key")) {
 
                             player.setCurrentRoom(e.getAdjacentRoom());
 
                             System.out.printf("You just travelled to [%s]!\n\n", player.getCurrentRoom().getRoomName());
                             System.out.println(player.getCurrentRoom().longDescription()); // Informs the player
-                        
+
                             return;
                         }
                     }
@@ -39,11 +41,11 @@ public class Go extends Command {
                     System.out.printf("The room [%s] is currently locked. Maybe come back with a key...\n", e.getAdjacentRoom());
                     return;
                 }
-                
-                // Hints for the player 
 
-                if (!CharacterConstants.LEFT_YOUR_ROOM && room.getRoomName().equals("Your Room") && e.getAdjacentRoom().equals("hallwayLow")) { 
-                    if(CharacterConstants.READ_LETTER_FROM_BRENT) {
+                // Hints for the player
+
+                if (!CharacterConstants.LEFT_YOUR_ROOM && room.getRoomName().equals("Your Room") && e.getAdjacentRoom().equals("hallwayLow")) {
+                    if (CharacterConstants.READ_LETTER_FROM_BRENT) {
                         Game.print("/bThe door still seems to be blocked by a massive object.");
                         Game.print("/bA note that you didn't notice before is stuck to the wall.");
                         Game.print("/bIt reads: /dbMake your way to the room with the family of ducks;");
@@ -61,11 +63,15 @@ public class Go extends Command {
                 System.out.printf("You just travelled to [%s]!\n\n", player.getCurrentRoom().getRoomName());
                 Game.print("/b" + player.getCurrentRoom().longDescription()); // Informing the player about it
 
-
-                if(!CharacterConstants.LEFT_YOUR_ROOM && e.getAdjacentRoom().equals("brentRoom")) {
+                if (!CharacterConstants.LEFT_YOUR_ROOM && e.getAdjacentRoom().equals("brentRoom")) {
                     Game.print("\n/bWhy would Brent's room be behind this wall? Why was my door locked?");
                     Game.print("/bWho killed Glenn? Consider talking to people in the hotel to gather /rclues/b.");
                     CharacterConstants.LEFT_YOUR_ROOM = true;
+                }
+
+                if (e.getAdjacentRoom().equals("privatePokerTable")) {
+                    Game.print("\n/bAs you enter the private poker table room, you notice a coat hanging on the wall.");
+                    Game.print("/bThere seems to be something inside the coat. Maybe you should check it.");
                 }
 
                 return;
